07e4f8f0b9a39adecffeec49cb394da61d1f2226
aa-r-ju <aarju3447@gmail.com>
2025-12-01 22:26:29 +0545

    feat: finished basic function of PBC course

--- index.js
+++ index.js
@@ -17,3 +17,38 @@ hello("David");
 hello("Nimit");
 let notAName = "world";
 hello(notAName);
+
+//function can have more that one parameters and aruguements
+function greeing(firstName, lastName) {
+  console.log("Hello", firstName, lastName);
+}
+greeing("Gaby", "Medina");
+//what happens if we do npt pass in an arguments
+greeing("Shila");
+
+//what happens if we pass in too many arguements
+greeing("Mina", "devi", "kc");
+
+//Returning values
+function getANum() {
+  return 10;
+}
+let aNum = getANum();
+console.log(aNum);
+
+function getFullName(firstName, lastName) {
+  return firstName + " " + lastName;
+  console.log("I will never run");
+}
+let first = "Alan";
+let last = "Turing";
+let fullName = getFullName(first, last);
+console.log(fullName);
+
+//what if we forget to return
+function getFullName1(firstName, lastName) {
+  let fullName = firstName + " " + lastName;
+  console.log("fullname:", fullName);
+}
+let returnedvalue = getFullName1("Julia", "Liuson");
+console.log(returnedvalue);
