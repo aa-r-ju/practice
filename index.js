5c2e05c37c88a0f3390beab7a25e0648b5766e7d
aa-r-ju <aarju3447@gmail.com>
2025-12-01 21:46:02 +0545

    feat: function calling multiple times

--- index.js
+++ index.js
@@ -2,4 +2,18 @@
 function imAFunction() {
   console.log("I am inside a function");
 }
+console.log("before the function");
 imAFunction();
+console.log("after the function");
+
+//using parameters and arguments to the function.
+function hello(name) {
+  console.log("Hello", name);
+}
+hello("sadie");
+
+//calling the function multiple times
+hello("David");
+hello("Nimit");
+let notAName = "world";
+hello(notAName);
