b98d8a44a9dfd6dd19468d0d6702061906b7e838
aa-r-ju <aarju3447@gmail.com>
2025-12-09 19:36:47 +0545

    feat: completed basic Scope

--- index.js
+++ index.js
@@ -1,34 +1,100 @@
-//simple for loops practice
-let letters = "world";
-console.log(letters[0]);
-console.log(letters[4]);
-console.log(letters[5]);
-console.log(letters.length);
-console.log(letters.toUpperCase());
-console.log(letters[2].toUpperCase());
-// console.log(letters[5].toUpperCase());
-//string in a for loop
-for (let i = 0; i < letters.length; i++) {
-  console.log(letters[i]);
-}
-let evenLetters = "";
-for (let i = 0; i < letters.length; i++) {
-  if (i % 2 === 0) {
-    evenLetters += letters[i];
-  }
-}
-console.log(evenLetters);
+// //basic Scope
+// let wow = "wow";
+// console.log("i can access wow because it is in scope:", wow);
 
-for (let i = 0; i < letters.length; i++) {
-  if (letters[i] === "r") {
-    continue;
-  }
-  console.log(letters[i]);
-}
+// let global = "ear";
+// console.log("outer", global);
 
-for (let i = 0; i < letters.length; i++) {
-  if (letters[i] === "r") {
-    break;
+// function funFunction() {
+//   console.log("inner", global);
+// }
+// funFunction();
+
+// // console.log(waitForIt);
+// let waitForIt = "Here i am";
+// console.log(waitForIt);
+
+// function happyFunction() {
+//   let message = "I am so happy";
+//   console.log(message);
+// }
+// happyFunction();
+// // console.log(message);
+
+// let message = "think globally";
+// function logAMessage() {
+//   let message = "Act locally";
+//   console.log(message);
+// }
+// logAMessage();
+
+// function logAMessage1() {
+//   let msg = "Act locally";
+//   console.log(message);
+// }
+// logAMessage1();
+
+// function logAMessage2(message) {
+//   console.log(message);
+// }
+// logAMessage2("act locally");
+
+// let globalVar = "global";
+// function outer() {
+//   let outerVar = "outer";
+//   function inner() {
+//     let innerVar = "inner";
+//     console.log(globalVar, outerVar, innerVar);
+//   }
+//   inner();
+// }
+
+// outer();
+
+// let collision = "Global";
+// function outer1(collision) {
+//   function inner1() {
+//     let collision = "inner";
+//     console.log(collision);
+//   }
+//   inner1();
+// }
+// outer1("outer");
+
+// function outer2(collision) {
+//   function inner2() {
+//     console.log(collision);
+//   }
+//   inner2();
+// }
+// outer2("outer");
+
+// function outer3() {
+//   function inner3() {
+//     console.log(collision);
+//   }
+//   inner3();
+// }
+// outer3("outer");
+
+function outer() {
+  let outerVar = "outer";
+  function inner(outerVar) {
+    let innerVar = "inner";
+    console.log(outerVar, innerVar);
   }
-  console.log(letters[i]);
+  inner();
+}
+outer();
+
+let alwaysTrue = true;
+function dontMindMe() {
+  // alwaysTrue = false;
+  let alwaysTrue = false;
+}
+dontMindMe();
+if (alwaysTrue) {
+  console.log("All is well");
+} else {
+  throw new Error("everything is broken");
 }
