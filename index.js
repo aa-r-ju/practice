86238fb915bddb0984e7e9a4e01cb551a32b50d4
aa-r-ju <aarju3447@gmail.com>
2025-12-23 22:33:55 +0545

    feat: use of forEach

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 //higher-order-function I
-function plus(num1, num2) {
-  return num1 + num2;
+let bridges = ["Brooklyn", "Golden Gate", "London"];
+function logUpperCase(string) {
+  console.log(string.toUpperCase());
 }
-function minus(num1, num2) {
-  return num1 - num2;
-}
-function calc(num1, operationFunc, num2) {
-  return operationFunc(num1, num2);
-}
-console.log(calc(10, plus, 20));
-console.log(calc(50, minus, 10));
+bridges.forEach(logUpperCase);
