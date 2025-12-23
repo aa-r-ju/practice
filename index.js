367cce6b1ef108f3412ddbf8878dbbd1799f7827
aa-r-ju <aarju3447@gmail.com>
2025-12-23 22:35:08 +0545

    feat: also printed index of element

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //higher-order-function I
 let bridges = ["Brooklyn", "Golden Gate", "London"];
-function logUpperCase(string) {
-  console.log(string.toUpperCase());
+function logUpperCase(string, idx) {
+  console.log(string, "is at index", idx);
 }
 bridges.forEach(logUpperCase);
