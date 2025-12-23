5b7665ba7f53e0f27124625b776290ebb07e0d63
aa-r-ju <aarju3447@gmail.com>
2025-12-23 22:11:14 +0545

    feat: say to all Hello

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 //higher-order-function I
-function addWorld(string) {
-  return string + " World";
+function sayToAll(names, sayWithNameFunc) {
+  for (let i = 0; i < names.length; i++) {
+    sayWithNameFunc(names[i]);
+  }
 }
-function callsWithHello(func) {
-  return func("Hello");
+let group = ["jane", "Jill", "Pip", "Mike"];
+
+function sayHelloWithName(name) {
+  console.log("Hello, ", name + "!");
 }
-let result = callsWithHello(addWorld);
-console.log(result);
+sayToAll(group, sayHelloWithName);
