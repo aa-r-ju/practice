ff883c3121ed13a205e2f4dc1695fd052ded3a96
aa-r-ju <aarju3447@gmail.com>
2025-12-23 21:34:01 +0545

    feat: calling each function  of an array using for loop.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 //higher-order-function I
-let amazingArray = [];
 function happyFunction() {
   console.log("I am Happy");
 }
-amazingArray.push(happyFunction);
-amazingArray.push(happyFunction);
-amazingArray.push(happyFunction);
-amazingArray.push(happyFunction);
-console.log(amazingArray);
+let amazingArray = [happyFunction, happyFunction, happyFunction];
+
+for (let i = 0; i < amazingArray.length; i++) {
+  let element = amazingArray[i];
+  element();
+}
