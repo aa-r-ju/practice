1a6545aa61c76c9c291b74c03aeac08946ab9ac3
aa-r-ju <aarju3447@gmail.com>
2026-02-10 17:57:26 +0545

    feat: testing makeCounter for practice

--- index.js
+++ index.js
@@ -19,3 +19,13 @@ function once(fn) {
 }
 
 let count = 0;
+
+function makeCounter() {
+  let count = 0;
+  return function () {
+    return ++count;
+  };
+}
+const counter = makeCounter();
+console.log(counter());
+console.log(counter());
