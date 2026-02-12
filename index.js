4c03f336928d19570e7198525c8c78b5ff512c23
aa-r-ju <aarju3447@gmail.com>
2026-02-12 14:03:42 +0545

    feat: toggleMaker

--- index.js
+++ index.js
@@ -11,5 +11,20 @@ function limitedCounter(val) {
     },
   };
 }
-let counter = limitedCounter(2);
-console.log(counter.increment());
+
+function toggleMaker(value) {
+  let result = value;
+  return {
+    toggle: function () {
+      result = !result;
+    },
+    getState: function () {
+      return result;
+    },
+  };
+}
+let toggle = toggleMaker(false);
+console.log(toggle.toggle());
+console.log(toggle.getState());
+console.log(toggle.toggle());
+console.log(toggle.getState());
