2fd22735aa9f2e1b0fb3cd237341c77af111e52c
aa-r-ju <aarju3447@gmail.com>
2026-02-12 20:48:51 +0545

    feat: testing createLogger for practice

--- index.js
+++ index.js
@@ -39,3 +39,26 @@ function createSecretCounter() {
     },
   };
 }
+
+function createLogger(num) {
+  let arr = [];
+  return {
+    log: function (value) {
+      if (arr.length < num) {
+        arr.push(value);
+      }
+    },
+    getLogs: function () {
+      return arr.slice();
+    },
+  };
+}
+let pum = createLogger(6);
+console.log(pum);
+console.log(pum.log("a"));
+console.log(pum.log("b"));
+console.log(pum.log("c"));
+console.log(pum.log("d"));
+console.log(pum.log("e"));
+console.log(pum.log("f"));
+console.log(pum.getLogs());
