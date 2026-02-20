70a55b9c60785d35b3a97002a0baf4bc157fff37
aa-r-ju <aarju3447@gmail.com>
2026-02-20 17:05:09 +0545

    feat: practising another RPN calculator

--- index.js
+++ index.js
@@ -48,3 +48,54 @@ calc.push(2);
 calc.divide();
 
 console.log(calc.value());
+
+class RPNCalculator1 {
+  constructor() {
+    this.stack = [];
+  }
+  push(num) {
+    this.stack.push(num);
+  }
+  value() {
+    return this.stack[this.stack.length - 1];
+  }
+  checkLength() {
+    if (this.stack.length < 2) {
+      throw "rpnCalculatorInstance is empty";
+    }
+  }
+  plus() {
+    this.checkLength();
+    let b = this.stack.pop();
+    let a = this.stack.pop();
+    let val = a + b;
+    this.stack.push(val);
+  }
+  minus() {
+    this.checkLength();
+    let b = this.stack.pop();
+    let a = this.stack.pop();
+    let val = a - b;
+    this.stack.push(val);
+  }
+  times() {
+    this.checkLength();
+    let b = this.stack.pop();
+    let a = this.stack.pop();
+    let val = a * b;
+    this.stack.push(val);
+  }
+  divide() {
+    this.checkLength();
+    let b = this.stack.pop();
+    let a = this.stack.pop();
+    let val = a / b;
+    this.stack.push(val);
+  }
+}
+
+let check = new RPNCalculator1();
+check.push(-2.5);
+check.push(3.5);
+check.plus();
+console.log(check.value());
