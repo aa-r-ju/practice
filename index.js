ac67aaedb3dd02cdb52f79b4290b38d2bfc45d41
aa-r-ju <aarju3447@gmail.com>
2026-02-23 22:30:43 +0545

    feat: declaire InvisibleWord class for parcrice

--- index.js
+++ index.js
@@ -1,25 +1,8 @@
-class Vehicle {
-  constructor(speed, fuelType) {
-    this.speed = speed;
-    this.fuelType = fuelType;
-  }
-
-  getDetails() {
-    return `Speed: ${this.speed}, Fuel: ${this.fuelType}`;
-  }
-}
-
-class Car extends Vehicle {
-  constructor(speed, fuelType, doors) {
-    super(speed, fuelType);
-    this.doors = doors;
-  }
-
-  getDetails() {
-    return `Speed: ${this.speed}, Fuel: ${this.fuelType},Doors: ${this.doors}`;
-  }
-  //we can also do this instead
-  getDetails() {
-    return `${super.getDetails()}, Doors: ${this.doors}`;
+class InvisibleWord {
+  constructor(word, remainingGuesses, guesses, status) {
+    this.word = word.toLowerCase().split("");
+    this.remainingGuesses = 5;
+    this.guesses = [];
+    this.status = "playing";
   }
 }
--- specs.js
+++ specs.js
@@ -1,53 +1,133 @@
-/* eslint-env jasmine */
-/* eslint-disable no-undef */
+/* eslint-disable no-useless-escape, no-unused-vars, no-prototype-builtins, no-undef */
 
-// Note: The ES2015 Class Keyword should be used.
-
-describe("Vehicle class", () => {
-  let myVehicle;
-
-  beforeEach(() => {
-    myVehicle = new Vehicle(120, "diesel");
+describe("InvisibleWord Class", () => {
+  it("attaches a constructor to InvisibleWord", () => {
+    expect(typeof InvisibleWord.constructor).toBe("function");
   });
 
-  it("is a Constructor Function", () => {
-    expect(typeof Vehicle).toBe("function");
+  it("instance has word, remainingGuesses, guesses, and status properties", () => {
+    const game = new InvisibleWord("banana");
+
+    expect(game.word).toEqual(["b", "a", "n", "a", "n", "a"]);
+    expect(game.remainingGuesses).toBe(5);
+    expect(game.guesses).toEqual([]);
+    expect(game.status).toBe("playing");
   });
 
-  it("should take speed and fuelType as parameters", () => {
-    expect(myVehicle.speed).toBe(120);
-    expect(myVehicle.fuelType).toBe("diesel");
+  describe("guessLetter method", () => {
+    it("is a prototype method", () => {
+      const game = new InvisibleWord("apple");
+      expect(game.hasOwnProperty("guessLetter")).toBe(false);
+    });
+
+    it("stores unique guesses only", () => {
+      const game = new InvisibleWord("apple");
+      game.guessLetter("a");
+      game.guessLetter("a");
+      game.guessLetter("p");
+
+      expect(game.guesses).toEqual(["a", "p"]);
+    });
+
+    it("reduces remainingGuesses for incorrect unique guesses", () => {
+      const game = new InvisibleWord("cat");
+
+      game.guessLetter("z");
+      game.guessLetter("x");
+      game.guessLetter("c");
+      game.guessLetter("v");
+
+      expect(game.remainingGuesses).toBe(3);
+    });
   });
 
-  it("should have a getDetails function on its prototype", () => {
-    expect(myVehicle.getDetails()).toBe("Speed: 120, Fuel: diesel");
-    expect(myVehicle.hasOwnProperty("getDetails")).toEqual(false);
+  describe("checkStatus method", () => {
+    it("is a prototype method", () => {
+      const game = new InvisibleWord("dog");
+      expect(game.hasOwnProperty("checkStatus")).toBe(false);
+    });
+
+    it("sets status to lost if remainingGuesses is 0", () => {
+      const game = new InvisibleWord("dog");
+
+      game.guessLetter("a");
+      game.guessLetter("b");
+      game.guessLetter("c");
+      game.guessLetter("e");
+      game.guessLetter("f");
+
+      game.checkStatus();
+
+      expect(game.status).toBe("lost");
+    });
+
+    it("sets status to won if all letters guessed", () => {
+      const game = new InvisibleWord("hi");
+
+      game.guessLetter("h");
+      game.guessLetter("i");
+
+      game.checkStatus();
+
+      expect(game.status).toBe("won");
+    });
   });
-});
 
-describe("Car class extends from Vehicle", () => {
-  let myCar;
+  describe("getPuzzle method", () => {
+    it('returns puzzle using "_" for hidden letters', () => {
+      const game = new InvisibleWord("apple");
+      expect(game.getPuzzle()).toBe("_____");
+    });
 
-  beforeEach(() => {
-    myCar = new Car(180, "petrol", 4);
+    it("reveals correct letters", () => {
+      const game = new InvisibleWord("apple");
+
+      game.guessLetter("p");
+
+      expect(game.getPuzzle()).toBe("_pp__");
+    });
+
+    it("handles spaces", () => {
+      const game = new InvisibleWord("ice cream");
+      expect(game.getPuzzle()).toBe("___ _____");
+    });
   });
 
-  it("is a Constructor Function", () => {
-    expect(typeof Car).toBe("function");
-  });
+  describe("getStatusMessage method", () => {
+    it("returns correct playing message", () => {
+      const game = new InvisibleWord("car");
 
-  it("should have speed, fuelType, and doors", () => {
-    expect(myCar.speed).toBe(180);
-    expect(myCar.fuelType).toBe("petrol");
-    expect(myCar.doors).toBe(4);
-  });
+      expect(game.getStatusMessage()).toBe(`Guesses left: 5
 
-  it("extends the Vehicle Class", () => {
-    expect(myCar instanceof Vehicle).toBe(true);
-    expect(myCar instanceof Car).toBe(true);
-  });
+=====
+`);
+    });
 
-  it("has access to the getDetails function from Vehicle", () => {
-    expect(myCar.getDetails()).toBe("Speed: 180, Fuel: petrol");
+    it("returns lost message with revealed word", () => {
+      const game = new InvisibleWord("hat");
+
+      game.guessLetter("x");
+      game.guessLetter("y");
+      game.guessLetter("z");
+      game.guessLetter("q");
+      game.guessLetter("w");
+      game.checkStatus();
+
+      expect(game.getStatusMessage()).toBe(`You lost! The word was "hat"
+
+  X
+=====
+`);
+    });
+
+    it("returns winning message", () => {
+      const game = new InvisibleWord("ok");
+
+      game.guessLetter("o");
+      game.guessLetter("k");
+      game.checkStatus();
+
+      expect(game.getStatusMessage()).toBe("You cracked the code!");
+    });
   });
 });
