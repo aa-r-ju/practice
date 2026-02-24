f380061cc6d04ba263ac75b5c00998a23134154d
aa-r-ju <aarju3447@gmail.com>
2026-02-24 11:18:57 +0545

    feat: checkStatus

--- index.js
+++ index.js
@@ -7,12 +7,32 @@ class InvisibleWord {
   }
 
   guessLetter(val) {
-    const letter = this.word.toLowerCase();
+    if (this.status !== "playing") return;
+
+    const letter = val.toLowerCase();
+
     if (!this.guesses.includes(letter)) {
       this.guesses.push(letter);
+
       if (!this.word.includes(letter)) {
-        this.remainingGuesses -= 1;
+        this.remainingGuesses--;
       }
+    }
+  }
+
+  checkStatus() {
+    if (this.remainingGuesses <= 0) {
+      this.status = "lost";
+    }
+    let winner = true;
+    for (let i = 0; i < this.word.length; i++) {
+      if (this.word[i] !== " " && !this.guesses.includes(this.word[i])) {
+        winner = false;
+        break;
+      }
+    }
+    if (winner) {
+      this.status = "won";
     }
   }
 }
