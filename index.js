f3f8bc0fbd7c011883bcd6b609b6b8f6e0bd4f86
aa-r-ju <aarju3447@gmail.com>
2026-02-27 14:45:12 +0545

    feat: characterFrequency

--- index.js
+++ index.js
@@ -15,3 +15,19 @@ function consonantsCount(str) {
       { total: 0 },
     );
 }
+
+function characterFrequency(str) {
+  return str
+    .toLowerCase()
+    .split("")
+    .reduce((acc, char) => {
+      if (char !== " ") {
+        acc[char] = (acc[char] || 0) + 1;
+      }
+      return acc;
+    }, {});
+}
+console.log(characterFrequency(""));
+console.log(characterFrequency("aab"));
+console.log(characterFrequency("a a b"));
+console.log(characterFrequency("Aa"));
