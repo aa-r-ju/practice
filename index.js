ca7ff681015ef928c823596d93c8d6c71e8661ca
aa-r-ju <aarju3447@gmail.com>
2026-03-03 14:34:48 +0545

    feat: isValidBrackets

--- index.js
+++ index.js
@@ -1,20 +1,43 @@
-function bracketCleaner(array) {
+// function bracketCleaner(array) {
+//   let obj = {
+//     "(": ")",
+//     "[": "]",
+//     "{": "}",
+//   };
+//   return array.reduce((acc, char) => {
+//     let last = acc[acc.length - 1];
+//     if (obj[last] === char) {
+//       acc.pop();
+//     } else {
+//       acc.push(char);
+//     }
+//     return acc;
+//   }, []);
+// }
+// console.log(bracketCleaner(["(", ")"]));
+// console.log(bracketCleaner(["[", "]"]));
+// console.log(bracketCleaner(["{", "}"]));
+// console.log(bracketCleaner(["(", "}"]));
+
+function isValidBrackets(array) {
   let obj = {
     "(": ")",
-    "[": "]",
     "{": "}",
+    "[": "]",
   };
-  return array.reduce((acc, char) => {
-    let last = acc[acc.length - 1];
-    if (obj[last] === char) {
-      acc.pop();
+  let stack = [];
+  for (let val of array) {
+    let last = stack[stack.length - 1];
+    if (obj[last] === val) {
+      stack.pop();
     } else {
-      acc.push(char);
+      stack.push(val);
     }
-    return acc;
-  }, []);
+  }
+  return stack.length === 0;
 }
-console.log(bracketCleaner(["(", ")"]));
-console.log(bracketCleaner(["[", "]"]));
-console.log(bracketCleaner(["{", "}"]));
-console.log(bracketCleaner(["(", "}"]));
+
+console.log(isValidBrackets(["(", "{", "}", ")"])); //true
+console.log(isValidBrackets(["(", "[", ")", "]"])); //false
+console.log(isValidBrackets(["{", "(", ")", "[", "]", "}"])); //true
+console.log(isValidBrackets(["(", "(", "[", "]", ")"])); // true
