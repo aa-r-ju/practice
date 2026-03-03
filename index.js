6b3642a615368eb6650b8c536a896c320eb6707e
aa-r-ju <aarju3447@gmail.com>
2026-03-03 18:12:21 +0545

    feat: cancelOutZeroSum

--- index.js
+++ index.js
@@ -42,7 +42,21 @@ function removeConsecutiveDuplicates(array) {
     return acc;
   }, []);
 }
-console.log(removeConsecutiveDuplicates([1, 1]));
-console.log(removeConsecutiveDuplicates([1, 1, 2, 2]));
-console.log(removeConsecutiveDuplicates([1, 2, 1]));
-console.log(removeConsecutiveDuplicates([1, 1, 2, 3, 3, 3, 2, 2]));
+
+function cancelOutZeroSum(array) {
+  return array.reduce((acc, char) => {
+    let last = acc[acc.length - 1];
+
+    if (last !== undefined && last + char === 0) {
+      acc.pop();
+    } else {
+      acc.push(char);
+    }
+    return acc;
+  }, []);
+}
+
+console.log(cancelOutZeroSum([3, -3]));
+console.log(cancelOutZeroSum([2, -2, 5]));
+console.log(cancelOutZeroSum([1, 2, -2, -1]));
+console.log(cancelOutZeroSum([5, 3, -3, 2]));
