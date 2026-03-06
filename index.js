4a2f9b2249063af6bbab42ae70c47bd91edd56a9
aa-r-ju <aarju3447@gmail.com>
2026-03-06 21:22:52 +0545

    feat: Array.prototype.onlyStrings

--- index.js
+++ index.js
@@ -6,5 +6,15 @@ Array.prototype.squareNumbers = function () {
   return newArr;
 };
 
-let nums = [1, 2, 3, 4];
-console.log(nums.squareNumbers());
+Array.prototype.onlyStrings = function () {
+  let newArr = [];
+  this.forEach((element) => {
+    if (typeof element === "string") {
+      newArr.push(element);
+    }
+  });
+  return newArr;
+};
+
+console.log([1, "hi", true, "hello", 5].onlyStrings());
+console.log([1, 2, 3].onlyStrings());
