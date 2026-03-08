6fd7a63e503e6f5b3a4f2f65bf40b8846d1e7538
aa-r-ju <aarju3447@gmail.com>
2026-03-08 22:33:56 +0545

    feat: ShoppingCart

--- index.js
+++ index.js
@@ -15,3 +15,31 @@ class Counter {
     this.count = 0;
   }
 }
+
+class ShoppingCart {
+  constructor() {
+    this.items = [];
+  }
+
+  addItem(name, price) {
+    this.items.push({ name, price });
+  }
+
+  getTotal() {
+    return this.items.reduce((acc, char) => {
+      acc += char.price;
+      return acc;
+    }, 0);
+  }
+
+  removeItem(name) {
+    this.items = this.items.filter((element) => element.name !== name);
+  }
+}
+
+let kk = new ShoppingCart();
+console.log(kk.addItem("Laptop", 1000), "lll");
+console.log(kk.addItem("Mouse", 50));
+console.log(kk.getTotal());
+console.log(kk.removeItem("Mouse"));
+console.log(kk);
