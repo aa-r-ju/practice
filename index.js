7fa67c7dc60c5a6a14e930074f35a938d4176876
aa-r-ju <aarju3447@gmail.com>
2026-03-13 16:18:10 +0545

    feat: once

--- index.js
+++ index.js
@@ -1,51 +1,20 @@
 function once(fn) {
-  let count = false;
+  let called = false;
   let result;
+
   return function (...args) {
-    if (!count) {
-      count = true;
-      result = fn(...args);
+    if (!called) {
+      called = true;
+      result = fn.apply(this, args);
     }
     return result;
   };
 }
 
-function after(num, fn) {
-  let count = 0;
-
-  return function (...args) {
-    count++;
-
-    if (count >= num) {
-      return fn(...args);
-    }
-  };
-}
-
-let counter = 0;
-
-function increment() {
-  counter++;
-  return counter;
-}
-
-function limitCalls(fn, num) {
-  let count = 0;
-  return function (...args) {
-    count++;
-
-    if (count <= num) {
-      return fn(...args);
-    } else {
-      return "Limit reached";
-    }
-  };
-}
-let myFunc;
-let add = (a, b) => a + b;
-
-myFunc = limitCalls(add, 3);
-console.log(myFunc(2, 3));
-console.log(myFunc(5, 3));
-console.log(myFunc(5, 4));
-console.log(myFunc(55, 88));
+let obj = {
+  value: 10,
+  add: once(function (x) {
+    return this.value + x;
+  }),
+};
+console.log(once(obj));
