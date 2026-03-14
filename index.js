4260f0ddab501949d3419a86b8cbb4ff9a37b96d
aa-r-ju <aarju3447@gmail.com>
2026-03-14 22:08:28 +0545

    feat: makeStack

--- index.js
+++ index.js
@@ -14,3 +14,24 @@ function makeCounter() {
     },
   };
 }
+
+function makeStack() {
+  let arr = [];
+  return {
+    push(num) {
+      arr.push(num);
+    },
+    pop(num) {
+      return arr.pop();
+    },
+    size() {
+      return arr.length;
+    },
+  };
+}
+
+let stack = makeStack();
+console.log(stack.push(9));
+console.log(stack.push(11));
+console.log(stack.pop());
+console.log(stack.size());
