69cdd8a9114e218aa56af41b1fbe1a66938fb54a
aa-r-ju <aarju3447@gmail.com>
2026-03-25 16:36:12 +0545

    feat: reduceRightReverse

--- index.js
+++ index.js
@@ -6,5 +6,15 @@ function reduceRight(arr, start, fn) {
   return acc;
 }
 
-let result = reduceRight([1, 2, 3, 4], 0, (acc, val) => acc + val);
-console.log(result);
+function reduceRightReverse(arr, container, fn) {
+  let acc = container;
+  for (let i = arr.length - 1; i >= 0; i--) {
+    acc = fn(acc, arr[i]);
+  }
+  return acc;
+}
+let kk = reduceRightReverse([1, 2, 3], [], (acc, val) => {
+  acc.push(val);
+  return acc;
+});
+console.log(kk);
