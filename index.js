dc7d9b1b9a5008afedc83e19fb5ac959194ed5c9
aa-r-ju <aarju3447@gmail.com>
2026-03-25 16:46:30 +0545

    feat: reduceRightFlatten

--- index.js
+++ index.js
@@ -13,8 +13,16 @@ function reduceRightReverse(arr, container, fn) {
   }
   return acc;
 }
-let kk = reduceRightReverse([1, 2, 3], [], (acc, val) => {
-  acc.push(val);
+
+function reduceRightFlatten(arr, con, fn) {
+  let acc = con;
+  for (let i = arr.length - 1; i >= 0; i--) {
+    acc = fn(acc, arr[i]);
+  }
   return acc;
+}
+
+let kk = reduceRightFlatten([1, [2, 3], 4], [], (acc, val) => {
+  return Array.isArray(val) ? acc.concat(val) : acc.concat(val);
 });
 console.log(kk);
