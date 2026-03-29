7049a164daf2542b9c805985f61cd02660a9ee56
aa-r-ju <aarju3447@gmail.com>
2026-03-29 22:15:22 +0545

    feat: sumEvenNumbers

--- index.js
+++ index.js
@@ -34,17 +34,31 @@ function getLongestString() {
   }
   return longest;
 }
-const result = getLongestString.call({
-  a: "hi",
-  b: "hello",
-  c: "hey",
+
+function sumEvenNumbers() {
+  let sum = 0;
+  for (let key in this) {
+    if (
+      this.hasOwnProperty(key) &&
+      typeof this[key] === "number" &&
+      this[key] % 2 === 0
+    ) {
+      sum += this[key];
+    }
+  }
+  return sum;
+}
+
+const result = sumEvenNumbers.call({
+  a: 2,
+  b: 3,
+  c: 4,
+  d: 5,
 });
 
-const result1 = getLongestString.call({
-  a: 123,
-  b: "JavaScript",
-  c: [1, 2],
+const result1 = sumEvenNumbers.call({
+  a: 1,
+  b: 3,
 });
-
-console.log(result);
 console.log(result1);
+console.log(result);
