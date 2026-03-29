86fe113af977439025f5810854b1efd63bfe5129
aa-r-ju <aarju3447@gmail.com>
2026-03-29 22:09:49 +0545

    feat: getLongestString

--- index.js
+++ index.js
@@ -21,15 +21,30 @@ function countStrings() {
   return totalStr;
 }
 
-// const result = countStrings.call({
-//   a: "hello",
-//   b: 2,
-//   c: "world",
-//   d: true,
-// });
-
-const result = countStrings.call({
-  a: 1,
-  b: 2,
+function getLongestString() {
+  let longest = "";
+  for (let key in this) {
+    if (
+      typeof this[key] === "string" &&
+      this.hasOwnProperty(key) &&
+      this[key].length > longest.length
+    ) {
+      longest = this[key];
+    }
+  }
+  return longest;
+}
+const result = getLongestString.call({
+  a: "hi",
+  b: "hello",
+  c: "hey",
 });
+
+const result1 = getLongestString.call({
+  a: 123,
+  b: "JavaScript",
+  c: [1, 2],
+});
+
 console.log(result);
+console.log(result1);
