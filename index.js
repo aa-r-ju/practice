b3dd891247c470edb84705e4dfe0e692531400ff
aa-r-ju <aarju3447@gmail.com>
2026-04-03 21:23:09 +0545

    feat: sum

--- index.js
+++ index.js
@@ -10,3 +10,13 @@ function range(start, end) {
   }
   return arr;
 }
+
+function sum(array) {
+  let sum = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+
+  return sum;
+}
